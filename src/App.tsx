fb5f7d992a1b6612f4c9288ae29a0b9a85cd654b
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-09-10 00:57:06 -0300

    useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
+import { LoggedIn } from './components/state/LoggedIn';
 
 function App() {
   
   return (
     <div className="App">
-      
+      <LoggedIn></LoggedIn>
     </div>
   );
 }
