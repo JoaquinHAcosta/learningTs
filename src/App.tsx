d06b6cf17c4d44eab96e8545d993e3d50bdf17c7
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-09-10 00:22:11 -0300

    Prop Types and Tips

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
-import { Container } from './components/Container';
 
 function App() {
   
   return (
     <div className="App">
-      <Container styles={{border: '1px solid black', padding: '1rem'}}/>
+      
     </div>
   );
 }
